a45ed8fa53cceb09910a6757bb7bbd6e58c73709
(martin selle) (atelier.martin.selle@gmail.com) <atelier.martin.selle@gmail.com>
2024-11-26 18:23:36 +0100

    EXO03-RAILS-WIKI

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+require 'faker'
+puts "Creating articles..."
+10.times do
+  Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(sentence_count: 10)
+  )
+end
+puts "Done!"
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
